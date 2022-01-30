5d718857cf55094ba9c7763f3c14b0c9f3239006
Valery Kovalev <kovalev.vxx@Valerys-MacBook-Pro.local>
2022-01-30 03:46:51 +0300

    ...

--- git_profile_seacher/js/app.js
+++ git_profile_seacher/js/app.js
@@ -23,40 +23,52 @@ function getReposList(link){
     const xhr = new XMLHttpRequest();
     xhr.open("GET", link, false);
     xhr.send();
-    const response = JSON.parse(xhr.responseText);
-    return response
-    // console.log(response);
-    // for (var i = 0; i < response.length; i++){
-    //     console.log(response[i])
-    // }
+    return JSON.parse(xhr.responseText)
 }
 
 function getGitHubInfo(json_api){
-    const login = json_api["items"][0]["login"]
-    const profile_photo = json_api["items"][0]["avatar_url"]
-    const profile_url = json_api["items"][0]["html_url"]
-    const repos = getReposList(json_api["items"][0]["repos_url"])
-    const info = {login: login, photo: profile_photo, url: profile_url, repos: repos}
-    draw(info)
-    return(info)
+    if (json_api["total_count"]===0){
+        alert("User not found")
+    } else {
+        const login = json_api["items"][0]["login"]
+        const profile_photo = json_api["items"][0]["avatar_url"]
+        const profile_url = json_api["items"][0]["html_url"]
+        const repos = getReposList(json_api["items"][0]["repos_url"])
+        const info = {login: login, photo: profile_photo, url: profile_url, repos: repos}
+        draw(info)
+        return(info)
+    }
 }
 
 function draw(info) {
-    var profile = document.querySelector(".profile")
+    let profile = document.querySelector(".profile")
     profile.innerHTML = ('<img src="'+ info.photo + '" alt="" class="avatar"><h1>'+ info.login +'</h1>')
     profile.href = info.url
 
-    var repo_text = document.querySelector(".repos h3")
+    let repo_text = document.querySelector(".repos h3")
     repo_text.innerHTML = ('<h3>Repositories</h3>')
 
     var repos_elements = document.querySelector(".repos-elements")
+    let lang_list = {"C":"c","C++":"cpp", "C#":"csharp", "CSS":"css", "Go":"go", "Python":"python", "Haskell":"haskell", "HTML": "html", "Java":"java", "JavaScript":"javascript", "Kotlin":"kotlin", "Lua": "lua", "Php":"php", "R":"r", "Swift":"swift", "Jupyter Notebook": "jupyter"}
     for (var i = 1; i < info.repos.length; i++){
-        repos_elements.innerHTML += ('<a href="'+info.repos[i]["html_url"]+'" class="repo" target="_blank">' +
-            '<ol><li>'+ info.repos[i]["name"]+'</li><li>'+ info.repos[i]["language"]+'</li>\n</ol>' +
-            '</a>');
+        let lang = info.repos[i]["language"] ?? ""
+        let logoPath = "../img/logos/none.png"
+        if (Object.keys(lang_list).includes(lang)){
+            logoPath = `../img/logos/${lang_list[lang]}.png`
+        }
+        let name = info.repos[i]["name"]
+        let url = info.repos[i]["html_url"]
+        repos_elements.innerHTML += (
+            `
+            <a href="${url}" class="repo" target="_blank">
+            <ol>
+            <li>${name}</li>
+            <li class="lang">${lang}</li>
+            </ol>
+            <img src="${logoPath}" alt="">
+            </a>
+            `
+        )
     }
-
-
-
 }
 
---
+++ notes/NOTES.md
@@ -0,0 +1,14 @@
+
+# Заметки
+
+Отличие var от let
+```JavaScript
+console.log(a);
+var a = 5;
+```
+>5
+```JavaScript
+console.log(a);
+let a = 5;
+```
+>Error
